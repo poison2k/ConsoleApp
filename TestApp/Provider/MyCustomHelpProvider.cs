f8187e804549e1a4f851af7e24617e90129e9fec
Sebastian Bünck <buenck@gmx.de>
2021-10-07 22:24:33 +0200

    Changed CommandDotNet override, now MyCustomProvider class override only needed parts

--- TestApp/Provider/MyCustomHelpProvider.cs
+++ TestApp/Provider/MyCustomHelpProvider.cs
@@ -11,7 +11,7 @@ using TestApp.Shared;
 
 namespace TestApp.Provider
 {
-    public class MyCustomHelpProvider : IHelpProvider
+    public class MyCustomHelpProvider : HelpTextProvider
     {
 
         private readonly IStringLocalizer _Localizer;
@@ -22,7 +22,7 @@ namespace TestApp.Provider
 
         private string? _appName;
 
-        public MyCustomHelpProvider(IStringLocalizerFactory stringLocalizerFactory, AppSettings appSettings, string appName = null)
+        public MyCustomHelpProvider(IStringLocalizerFactory stringLocalizerFactory, AppSettings appSettings, string appName = null) : base(appSettings, appName)
         {
             _appName = appName;
             _appHelpSettings = appSettings.Help;
@@ -30,7 +30,7 @@ namespace TestApp.Provider
             _Localizer = _LocalizerFactory.Create(typeof(CommandDotNetLocalization));
         }
 
-        public virtual string GetHelpText(Command command) =>
+        public override string GetHelpText(Command command) =>
             JoinSections(
                 (null, CommandDescription(command)),
                 (CheckForLocalisation(CommandDotNetConsts.Usage), SectionUsage(command)),
@@ -40,26 +40,10 @@ namespace TestApp.Provider
                 (CheckForLocalisation(CommandDotNetConsts.Commands), SectionSubcommands(command)),
                 (null, ExtendedHelpText(command)));
 
-        /// <summary>returns the body of the usage section</summary>
-        protected virtual string? SectionUsage(Command command)
-        {
-            var usage = PadFront(command.Usage)
-                           ?? $"{PadFront(AppName(command))}{PadFront(CommandPath(command))}"
-                           + $"{PadFront(UsageSubcommand(command))}{PadFront(UsageOption(command))}{PadFront(UsageOperand(command))}"
-                           + (command.ArgumentSeparatorStrategy == ArgumentSeparatorStrategy.PassThru ? " [[--] <arg>...]" : null);
-            return CommandReplacements(command, usage);
-        }
 
-        protected virtual string AppName(Command command)
-        {
-            return _appName ??= _appHelpSettings.GetAppName();
-        }
-
-        /// <summary>The current command and it's parents.  aka bread crumbs</summary>
-        protected virtual string CommandPath(Command command) => command.GetPath();
 
         /// <summary>How operands are shown in the usage example</summary>
-        protected virtual string? UsageOperand(Command command)
+        protected override string? UsageOperand(Command command)
         {
             if (!command.Operands.Any())
             {
@@ -98,173 +82,49 @@ namespace TestApp.Provider
         }
 
         /// <summary>How options are shown in the usage example</summary>
-        protected virtual string? UsageOption(Command command) =>
+        protected override string? UsageOption(Command command) =>
             command.Options.Any(o => !o.Hidden)
                 ? CommandDotNetConsts.OptionsField
                 : null;
 
         /// <summary>How subcommands are shown in the usage example</summary>
-        protected virtual string? UsageSubcommand(Command command) =>
+        protected override string? UsageSubcommand(Command command) =>
             command.Subcommands.Any()
                 ? CommandDotNetConsts.CommandField
                 : null;
 
-        protected virtual string? ExtendedHelpText(Command command)
+        protected override string? ExtendedHelpText(Command command)
         {
             return CommandReplacements(command, CheckForLocalisation(command.ExtendedHelpText)); 
         }
-
-        /// <summary>returns the body of the options section</summary> 
-        protected virtual string? SectionOptions(Command command, bool includeInterceptorOptionsForExecutableCommands)
-        {
-            var options = command.Options
-                .Where(o => !o.Hidden)
-                .Where(o => includeInterceptorOptionsForExecutableCommands
-                    ? (command.IsExecutable && o.IsInterceptorOption)
-                    : !(command.IsExecutable && o.IsInterceptorOption))
-                .OrderBy(o => o.IsMiddlewareOption)
-                .ThenBy(o => o.IsInterceptorOption)
-                .ToCollection();
-
-            return SectionArguments(command, options);
-        }
-
-        /// <summary>returns the body of the operands section</summary>
-        protected virtual string? SectionOperands(Command command) =>
-            SectionArguments(command, command.Operands.ToCollection());
-
-        /// <summary>returns the body of an arguments section</summary>
-        protected virtual string? SectionArguments<T>(Command command, ICollection<T> arguments)
-            where T : IArgument
-        {
-            if (!arguments.Any())
-            {
-                return null;
-            }
-
-            var helpValues = BuildArgumentHelpValues(arguments);
-            var templateMaxLength = helpValues.Max(a => a.Template?.Length) ?? 0;
-            var displayNameMaxLength = helpValues.Max(a => a.TypeName?.Length) ?? 0;
-
-            var sb = new StringBuilder();
-            foreach (var helpValue in helpValues)
-            {
-                if (sb.Length > 0)
-                {
-                    sb.AppendLine();
-                }
-
-                new[]
-                    {
-                        Row(
-                            (templateMaxLength, helpValue.Template),
-                            (displayNameMaxLength, helpValue.TypeName),
-                            (-1, helpValue.DefaultValue)),
-                        Row(helpValue.Description),
-                        Row(helpValue.AllowedValues)
-                    }
-                    .Where(r => !r.IsNullOrWhitespace())
-                    .ForEach(r => sb.AppendLine(r));
-            }
-
-            return sb.ToString();
-        }
-
-        /// <summary>returns the body of the subcommands section</summary>
-        protected virtual string? SectionSubcommands(Command command)
-        {
-            var commands = command.Subcommands.ToCollection();
-
-            if (!commands.Any())
-            {
-                return null;
-            }
-
-            var helpValues = BuildCommandHelpValues(commands);
-            var maxCmdLen = helpValues.Max(c => c.Name?.Length) ?? 0;
-
-            var sb = new StringBuilder();
-            foreach (var helpValue in helpValues.OrderBy(c => c.Name))
-            {
-                sb.AppendLine(Row((maxCmdLen, helpValue.Name), (-1, helpValue.Description)));
-            }
-
-            var subcommandHelpHint = SubcommandHelpHint(command);
-            if (!subcommandHelpHint.IsNullOrWhitespace())
-            {
-                sb.AppendLine();
-                sb.Append(subcommandHelpHint);
-            }
-
-            return sb.ToString();
-        }
-
+ 
         /// <summary>Hint displayed in the subcommands section for getting help for a subcommand.</summary>
-        protected virtual string? SubcommandHelpHint(Command command) =>
+        protected override string? SubcommandHelpHint(Command command) =>
             String.Format(_Localizer.GetString(CommandDotNetConsts.SubcommandHelpHint), AppName(command),PadFront(CommandPath(command)), Constants.HelpOptionName);
 
-        protected virtual string CommandName(Command command) => command.Name;
+        private static string? PadFront(string? value) =>
+           value.IsNullOrWhitespace() ? null : " " + value;
 
-        protected virtual string? CommandDescription(Command command)
+        protected override string? CommandDescription(Command command)
         {
             return CheckForLocalisation(command.Description.UnlessNullOrWhitespace());
         }
 
-        protected virtual string? ArgumentName<T>(T argument) where T : IArgument =>
-            argument.SwitchFunc(
-                operand => operand.Name,
-                option => OptionTemplate.Build(option.LongName, option.ShortName));
-
-        protected virtual string? ArgumentTypeName<T>(T argument) where T : IArgument =>
-            argument.TypeInfo.DisplayName.UnlessNullOrWhitespace(n => $"<{n.ToUpperInvariant()}>");
-
-        protected virtual string? ArgumentDescription<T>(T argument) where T : IArgument
+        protected override string? ArgumentDescription<IArgument>(IArgument argument) 
         {
             return CheckForLocalisation(argument.Description.UnlessNullOrWhitespace());
         }
 
-        protected virtual string? ArgumentArity<T>(T argument) where T : IArgument =>
-            (argument.Arity.AllowsMany() ? " ("+CheckForLocalisation(CommandDotNetConsts.Multiple)+")" : "");
-
-        /// <summary>Returns a comma-separated list of the allowed values</summary>
-        protected virtual string? ArgumentAllowedValues<T>(T argument) where T : IArgument =>
-            argument.AllowedValues?.ToCsv(", ").UnlessNullOrWhitespace(v => $"{CheckForLocalisation(CommandDotNetConsts.AllowedValues)}: {v}");
-
-        /// <summary></summary>
-        protected virtual string? ArgumentDefaultValue(IArgument argument)
+        protected override string? ArgumentArity<IArgument>(IArgument argument)
         {
-            object? defaultValue = argument.Default?.Value;
-
-            return defaultValue.IsNullValue()
-                ? null
-                : $"[{defaultValue!.ValueToString(argument)}]";
+            return (argument.Arity.AllowsMany() ? " (" + CheckForLocalisation(CommandDotNetConsts.Multiple) + ")" : "");
         }
 
-        /// <summary>Row with default indent of 2 spaces</summary>
-        protected virtual string? Row(string? cell, int indent = 2) => $"{new string(' ', indent)}{cell}".TrimEnd();
-
-        /// <summary>Indents the row and aligns the cells</summary>
-        protected virtual string? Row(params (int maxLength, string? value)[] cells) =>
-            Row(cells.Select(c => c.maxLength > 0
-                    ? string.Format($"{{0, -{c.maxLength + 2}}}", c.value)
-                    : c.value)
-                .ToCsv(""));
-
-        /// <summary>Formats a section header.  Default appends line endings except for Usage</summary>
-        protected virtual string? FormatSectionHeader(string header)
-            => CheckForLocalisation(CommandDotNetConsts.Usage.ToLower()).Equals(header, StringComparison.OrdinalIgnoreCase)
-                    ? $"{header}:"
-                    : $"{header}:{Environment.NewLine}{Environment.NewLine}";
-
-        /// <summary>Joins the content into a single string, with headers and sections</summary>
-        protected virtual string JoinSections(params (string? header, string? body)[] sections) =>
-            sections
-                .Where(s => !s.body.IsNullOrWhitespace())
-                .Select(s => $"{(s.header.IsNullOrWhitespace() ? null : FormatSectionHeader(s.header!))}{s.body!.Trim('\n', '\r')}")
-                .ToCsv($"{Environment.NewLine}{Environment.NewLine}");
-
-        private static string? PadFront(string? value) =>
-            value.IsNullOrWhitespace() ? null : " " + value;
+        /// <summary>Returns a comma-separated list of the allowed values</summary>
+        protected override string? ArgumentAllowedValues<IArgument>(IArgument argument)
+        {
+            return argument.AllowedValues?.ToCsv(", ").UnlessNullOrWhitespace(v => $"{CheckForLocalisation(CommandDotNetConsts.AllowedValues)}: {v}");
+        }
 
         private class CommandHelpValues
         {
@@ -277,9 +137,6 @@ namespace TestApp.Provider
                 Description = description;
             }
         }
-
-        private ICollection<CommandHelpValues> BuildCommandHelpValues(IEnumerable<Command> commands) =>
-            commands.Select(c => new CommandHelpValues(CommandName(c), CommandDescription(c))).ToCollection();
 
         private string? CommandReplacements(Command command, string? text) => text?
             .Replace("%UsageAppName%", AppName(command))
